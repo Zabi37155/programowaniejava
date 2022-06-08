7957992ca37aca4f6faa6db22044c5fd675bf7f9
Zabi37155 <101585360+Zabi37155@users.noreply.github.com>
2022-06-08 19:29:23 +0200

    Add files via upload
    Lab1

---
+++ Lab1/src/main/java/pl/lublin/wsei/java/cwiczenia/lab1/Main.java
@@ -0,0 +1,20 @@
+package pl.lublin.wsei.java.cwiczenia.lab1;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1=0, num2=0;
+        do {
+
+            System.out.print("Podaj liczbe, ktora mam dodac:");
+            num1 = input.nextInt();
+            num2 = input.nextInt();
+            if (num1 == 0) || (num2 == 0)break;
+
+            System.out.printf("Wynik dodawania %d+%d = %d%n", num1, num2, num1 + num2);
+
+
+        } while (true);
+    }
+}
